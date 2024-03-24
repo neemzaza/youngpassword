9385b34321b508d0962a4d96cfec2f512761a9e0
neemzaza <keng.442511@gmail.com>
2024-03-24 22:48:45 +0700

    24/3/2024

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
   const link3 = "";
   const link4 = "";
 
-  const hintsub1 = "JS"; // JS // Airwavy
-  const hintsub2 = "###"; // String // NP Band
-  const hintsub3 = "###"; // YouTube // iPhone 13
+  const hintsub1 = "Airwavy"; // JS // Airwavy
+  const hintsub2 = "NP Band"; // String // NP Band
+  const hintsub3 = "4/5 เก่า"; // YouTube // iPhone 13
   const hintsub4 = "###"; // Man // Web App 1st 
 
-  const date = "21/3/2024";
+  const date = "24/3/2024";
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -28,7 +28,7 @@ export default function Home() {
         
       </div>
 
-      <div className="relative place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* <div className="relative place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <label>NOTE จากพี่รหัส</label>
         <h1 className="text-4xl">พี่ไม่สะดวกติดต่อทางเบอร์อ่ะ ติดต่อยาก รบกวนติดต่อ</h1>
         <br/>
@@ -39,7 +39,7 @@ export default function Home() {
             <li>หรือถ้าไม่ได้จริงๆ รบกวนเขียน Email มาหาพี่หน่อยนะ : <Link href="mailto:webgoodd@gmail.com">webgoodd@gmail.com</Link></li>
           </ul>
         </h1>
-      </div>
+      </div> */}
 
       <br/>
 
